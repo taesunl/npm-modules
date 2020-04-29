edb4fbbb75f5252202bbad17e0a38d2acd894e05
Taesun Lee <taesun1003@hanmail.net>
2020-04-29 16:25:20 +0900

    Initial release

---
+++ NodeTutorial.js
@@ -0,0 +1,10 @@
+var Tutor=require('./Tutorial.js');
+exports.NodeTutorial=function()
+{
+    console.log("Node Tutorial")
+    this.pTutor = function ()
+    {
+        var PTutor=Tutor
+        PTutor.tutorial();
+    }
+}
---
+++ app.js
@@ -0,0 +1,3 @@
+var localTutor=require('./NodeTutorial.js');
+var tut = new localTutor.NodeTutorial();  // Create and save object
+tut.pTutor();  // Call function on object
